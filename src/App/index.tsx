6c011eb8ee12bd656c5b709dd6b9fc081fd7eaa5
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-02-26 14:45:42 +0000

    feat: subscribe to uiControlChanges to test

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AmpEnvelope } from '../components/AmpEnv';
 import { Filter } from '../components/Filter';
 import { FilterEnvelope } from '../components/FilterEnvelope';
 import { Oscillator } from '../components/Oscillator';
 import { SynthUI } from '../components/SynthUI';
-import useMidiAccess from '../hooks/useMidiAccess';
+import { useMidiAccess } from '../hooks/useMidiAccess';
 import './index.module.css';
+
+interface UIControlChange {
+  groupName: string;
+  parameter: string;
+  value: number;
+}
 
 function App() {
   const [focus, setFocus] = useState('oscillator');
-  useMidiAccess();
+useMidiAccess();
 
   function updateFocus(id: string) {
     setFocus(id);
   }
+
+  function onUIControlChange(message: string, payload: UIControlChange) {
+    console.log('onUIControlChange', payload)
+  }
+
+  useEffect(() => {
+    const sub = PubSub.subscribe('uiControlChange', onUIControlChange)
+
+    return () => { PubSub.unsubscribe(sub) };
+  }, [])
 
   return (
     <div className="App">
