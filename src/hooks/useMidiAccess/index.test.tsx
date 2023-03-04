c0422ab6f17b700aef7d0674ddcb4c497615538e
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-04 16:52:56 +0000

    test: Add tests for useMidiAccess hook

--- src/hooks/useMidiAccess/index.test.tsx
+++ src/hooks/useMidiAccess/index.test.tsx
@@ -1,8 +1,7 @@
 import { useEffect } from 'react';
 import { useMidiAccess } from ".";
-import { render, screen } from '@testing-library/react';
-import { vi } from 'vitest';
-
+import { render, screen, waitFor } from '@testing-library/react';
+import WMT from 'web-midi-test';
 
 const WrapperComponent = () => {
     const {
@@ -13,33 +12,52 @@ const WrapperComponent = () => {
         midiOutputs
     } = useMidiAccess();
 
-    useEffect(() => {
-        console.log(midiAccess);
-        
-    }, [isRequesting, midiAccess])
-
     return (
-        <div>
-            {
-                isRequesting
-                    ? <div>requesting MIDI access</div>
-                    : <div>MIDI access granted</div>
-            }
-        </div>
+        <>
+            <div>
+                {isRequesting
+                    ? "requesting MIDI access"
+                    : "MIDI access granted"
+                }
+            </div>
+            <div>
+                {midiAccessError
+                    ? "MIDI Access Error"
+                    : "No MIDI Access Error"
+                }
+            </div>
+        </>
     )
 }
 
 
-describe('useMidiAccess', () => {    
+describe('useMidiAccess', () => {
     it('has an initial isRequesting state of "true"', async () => {
         render(<WrapperComponent />);
 
-        expect(screen.getByText(/requesting MIDI access/i)).toBeInTheDocument();
+        await waitFor(() => {
+            expect(screen.getByText(/requesting MIDI access/i)).toBeInTheDocument()
+        });
+    });
+
+    it('has a defined midiAccess if the request for MIDI access failed', async () => {
+        WMT.midi = false;
+        
+        render(<WrapperComponent />);
+
+        await waitFor(() => {
+            expect(screen.getByText(/MIDI Access Error/i)).toBeInTheDocument()
+        });
+
+        WMT.midi = true;
     });
 
     it('has an isRequesting state of "false" when MIDI access has been granted', async () => {
+
         render(<WrapperComponent />);
 
         expect(await screen.findByText(/MIDI access granted/i)).toBeInTheDocument();
     });
+
+    
 });
