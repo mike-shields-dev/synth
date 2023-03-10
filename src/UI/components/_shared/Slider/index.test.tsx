63eb2381b73ae496dd0a5b7916160285f4b4ebc0
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-10 09:38:33 +0000

    test: Add to and refactor Slider tests

--- src/UI/components/_shared/Slider/index.test.tsx
+++ src/UI/components/_shared/Slider/index.test.tsx
@@ -1,11 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { vi } from 'vitest';
 import { Slider } from '.';
+import { MIDI_CC } from '../../../../PubSub/topics';
 
 const validProps = {
     controlChangeNumber: 70,
     group: 'groupName',
     initVal: 240,
+    updateSynth: vi.fn(),
     isFocused: true,
     parameter: 'parameter',
     scalers: {
@@ -44,6 +46,13 @@ describe('Slider', () => {
             .toEqual('127');
     });
 
+    it('renders a range input with the correctly scaled value', () => {
+        const slider = screen.getByRole('slider', { name: 'Parameter' });
+        const scaledValue = validProps.scalers.in(validProps.initVal)
+
+        expect(slider).toHaveValue(`${scaledValue}`);
+    });
+
     it('renders an output element', () => {
         expect(screen.getByRole('status')).toBeInTheDocument();
     });
@@ -54,7 +63,36 @@ describe('Slider', () => {
             .toBe(`${validProps.group}-${validProps.parameter}`);
     });
 
-    it('renders an output element that has a scaled value', () => {
-        // to do...
+    it('renders an output element that has a scaled value', async () => {
+        const slider = screen.getByRole('slider', { name: 'Parameter' });
+        const newValue = 23;
+        const scaledValue = validProps.scalers.out(newValue)
+
+        fireEvent.change(slider, { target: { value: newValue } });
+
+        expect(await screen.findByText(scaledValue)).toBeInTheDocument();
+    });
+
+    it('invokes the provided updateSynth function when the user moves the slider', async () => {
+        const slider = screen.getByRole('slider', { name: 'Parameter' });
+        const newValue = 64;
+        
+        fireEvent.change(slider, { target: { value: newValue } });
+
+        expect(validProps.updateSynth).toHaveBeenCalledTimes(1);
+        expect(validProps.updateSynth).toHaveBeenCalledWith(newValue);
+    });
+
+    it('updates the slider value when a MIDI control change is received', async () => {
+        const newValue = 88;
+
+        PubSub.publish(MIDI_CC, {
+            controlChangeNumber: validProps.controlChangeNumber,
+            value: newValue,
+        });
+
+        await waitFor(() => {
+            expect(screen.getByRole('slider')).toHaveValue(`${newValue}`);
+        });
     });
 });
