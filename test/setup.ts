48e95efb5a2787bf4ad5e9d89a1e13b9af04191c
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-10 10:53:49 +0000

    chore: Mock WAAPI in test/setup.ts

--- test/setup.ts
+++ test/setup.ts
@@ -2,13 +2,16 @@ import matchers from '@testing-library/jest-dom/matchers';
 import { cleanup } from '@testing-library/react';
 import { afterEach, expect, vi } from 'vitest';
 import WMT from 'web-midi-test';
+import * as WATAPI from 'web-audio-test-api';
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
 
-beforeEach(() => {
-  global.navigator.requestMIDIAccess = WMT.requestMIDIAccess; 
+beforeAll(() => {
+  globalThis.AudioContext = WATAPI.AudioContext;
+  globalThis.navigator.requestMIDIAccess = WMT.requestMIDIAccess;
   vi.mock("pubsub-js", async (module) => await module());
+  vi.mock('tone', async (module) => await module());
 });
 
 // runs a cleanup after each test case (e.g. clearing jsdom)
