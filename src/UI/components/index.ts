5da6c9603c9533c863f6ece5b0eae7fe6dc5f316
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-03-11 21:51:38 +0000

    refactor: Add UIKeyboard to components index file

--- src/UI/components/index.ts
+++ src/UI/components/index.ts
@@ -3,3 +3,4 @@ export { Filter } from './Filter';
 export { FilterEnvelope } from './FilterEnvelope';
 export { Oscillator } from './Oscillator';
 export { SynthUI } from './SynthUI';
+export { UIKeyboard } from './UIKeyboard';
