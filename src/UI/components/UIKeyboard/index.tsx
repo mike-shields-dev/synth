5386e65e4d2acf0bb9f2fafe1b7fc4a72f0492cb
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-28 09:42:59 +0100

    refactor: Format file

--- src/UI/components/UIKeyboard/index.tsx
+++ src/UI/components/UIKeyboard/index.tsx
@@ -1,148 +1,166 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
-    NoteOff, NoteOffSubscriber, NoteOn, NoteOnSubscriber, PitchBendSubscriber, publishNoteOff, publishNoteOn, publishPitchBend
+  NoteOff,
+  NoteOffSubscriber,
+  NoteOn,
+  NoteOnSubscriber,
+  PitchBendSubscriber,
+  publishNoteOff,
+  publishNoteOn,
+  publishPitchBend,
 } from "../../../PubSub";
-import { publishOctaveChange } from '../../../PubSub/OctaveChange';
-import { octaveToNoteOffset } from '../../../utils/Scalers';
-import css from './index.module.css';
+import { publishOctaveChange } from "../../../PubSub/OctaveChange";
+import { octaveToNoteOffset } from "../../../utils/Scalers";
+import css from "./index.module.css";
 
-type Key = { name: string, leftOffset: number, value: number, className: string }
+type Key = {
+  name: string;
+  leftOffset: number;
+  value: number;
+  className: string;
+};
 
 const keys: Key[] = [
-    { name: 'C', leftOffset: 0, value: 60, className: "MajorKey" },
-    { name: 'Db', leftOffset: 0.75, value: 61, className: "MinorKey" },
-    { name: 'D', leftOffset: 1, value: 62, className: "MajorKey" },
-    { name: 'Eb', leftOffset: 1.75, value: 63, className: "MinorKey" },
-    { name: 'E', leftOffset: 2, value: 64, className: "MajorKey" },
-    { name: 'F', leftOffset: 3, value: 65, className: "MajorKey" },
-    { name: 'Gb', leftOffset: 3.75, value: 66, className: "MinorKey" },
-    { name: 'G', leftOffset: 4, value: 67, className: "MajorKey" },
-    { name: 'Ab', leftOffset: 4.75, value: 68, className: "MinorKey" },
-    { name: 'A', leftOffset: 5, value: 69, className: "MajorKey" },
-    { name: 'Bb', leftOffset: 5.75, value: 70, className: "MinorKey" },
-    { name: 'B', leftOffset: 6, value: 71, className: "MajorKey" },
+  { name: "C", leftOffset: 0, value: 60, className: "MajorKey" },
+  { name: "Db", leftOffset: 0.75, value: 61, className: "MinorKey" },
+  { name: "D", leftOffset: 1, value: 62, className: "MajorKey" },
+  { name: "Eb", leftOffset: 1.75, value: 63, className: "MinorKey" },
+  { name: "E", leftOffset: 2, value: 64, className: "MajorKey" },
+  { name: "F", leftOffset: 3, value: 65, className: "MajorKey" },
+  { name: "Gb", leftOffset: 3.75, value: 66, className: "MinorKey" },
+  { name: "G", leftOffset: 4, value: 67, className: "MajorKey" },
+  { name: "Ab", leftOffset: 4.75, value: 68, className: "MinorKey" },
+  { name: "A", leftOffset: 5, value: 69, className: "MajorKey" },
+  { name: "Bb", leftOffset: 5.75, value: 70, className: "MinorKey" },
+  { name: "B", leftOffset: 6, value: 71, className: "MajorKey" },
 ];
 
 function UIKeyboard() {
-    const defaultPitchBend = 8192;
-    const [pitchBend, setPitchBend] = useState(defaultPitchBend);
-    const [octave, setOctave] = useState(0);
-    const [activeNotes, setActiveNotes] = useState<number[]>([]);
+  const defaultPitchBend = 8192;
+  const [pitchBend, setPitchBend] = useState(defaultPitchBend);
+  const [octave, setOctave] = useState(0);
+  const [activeNotes, setActiveNotes] = useState<number[]>([]);
 
-    function onNote(e: React.MouseEvent<HTMLButtonElement>) {
-        e.preventDefault();
-        const noteNumber = +e.currentTarget.value;
-
-        if (['click', 'mousedown'].includes(e.type)) {
-            return publishNoteOn({
-                noteNumber: noteNumber + octaveToNoteOffset(octave),
-                velocity: 80,
-            })
-        }
-        if (['mouseup', 'mouseleave'].includes(e.type)) {
-            return publishNoteOff({
-                noteNumber: noteNumber + octaveToNoteOffset(octave),
-            })
-        }
+  function onNote(e: React.MouseEvent<HTMLButtonElement>) {
+    const noteNumber = +e.currentTarget.value;
+    if (["mousedown"].includes(e.type)) {
+      return publishNoteOn({
+        noteNumber: noteNumber + octaveToNoteOffset(octave),
+        velocity: 80,
+      });
     }
 
-    function onPitchBend(e: React.ChangeEvent<HTMLInputElement> | React.MouseEvent<HTMLInputElement>) {
-        const { value } = e.currentTarget;
-        
-        if (e.type === 'change') {
-            publishPitchBend(+value);
-            setPitchBend(+value);
-        }
+    if (["mouseup", "mouseleave"].includes(e.type)) {
+      return publishNoteOff({
+        noteNumber: noteNumber + octaveToNoteOffset(octave),
+      });
+    }
+  }
 
-        if (e.type === 'mouseup') {
-            publishPitchBend(defaultPitchBend);
-            setPitchBend(defaultPitchBend);
-        }
+  function onPitchBend(
+    e: React.ChangeEvent<HTMLInputElement> | React.MouseEvent<HTMLInputElement>
+  ) {
+    const { value } = e.currentTarget;
+
+    if (e.type === "change") {
+      publishPitchBend(+value);
+      setPitchBend(+value);
     }
 
-    function onPublishedNoteOn(TOPIC: string, data: NoteOn) {
-        const noteNumber = data.noteNumber;
-
-        setActiveNotes((prevActiveNotes) => [...new Set([noteNumber, ...prevActiveNotes])]);
+    if (e.type === "mouseup") {
+      publishPitchBend(defaultPitchBend);
+      setPitchBend(defaultPitchBend);
     }
+  }
 
-    function onPublishedNoteOff(TOPIC: string, data: NoteOff) {
-        const noteNumber = data.noteNumber;
+  function onPublishedNoteOn(TOPIC: string, data: NoteOn) {
+    const noteNumber = data.noteNumber;
 
-        setActiveNotes(prevActiveNotes =>
-            prevActiveNotes.filter(activeNote => activeNote !== noteNumber)
-        );
-    }
+    setActiveNotes((prevActiveNotes) => [
+      ...new Set([noteNumber, ...prevActiveNotes]),
+    ]);
+  }
 
-    function onPublishedPitchBend(TOPIC: string, data: number) {
-        setPitchBend(data);
-    }
+  function onPublishedNoteOff(TOPIC: string, data: NoteOff) {
+    const noteNumber = data.noteNumber;
 
-    useEffect(() => {
-        const noteOnSubscriber = new NoteOnSubscriber(onPublishedNoteOn);
-        const noteOffSubscriber = new NoteOffSubscriber(onPublishedNoteOff);
-        const pitchBendSubscriber = new PitchBendSubscriber(onPublishedPitchBend);
+    setActiveNotes((prevActiveNotes) =>
+      prevActiveNotes.filter((activeNote) => activeNote !== noteNumber)
+    );
+  }
 
-        return () => {
-            noteOnSubscriber.unsubscribe();
-            noteOffSubscriber.unsubscribe();
-            pitchBendSubscriber.unsubscribe();
-        }
-    }, []);
+  function onPublishedPitchBend(TOPIC: string, data: number) {
+    setPitchBend(data);
+  }
 
-    function onOctave(e: React.ChangeEvent<HTMLInputElement>) {
-        const { value } = e.target;
+  useEffect(() => {
+    const noteOnSubscriber = new NoteOnSubscriber(onPublishedNoteOn);
+    const noteOffSubscriber = new NoteOffSubscriber(onPublishedNoteOff);
+    const pitchBendSubscriber = new PitchBendSubscriber(onPublishedPitchBend);
 
-        setOctave(+value)
-        publishOctaveChange(+value);
-    }
+    return () => {
+      noteOnSubscriber.unsubscribe();
+      noteOffSubscriber.unsubscribe();
+      pitchBendSubscriber.unsubscribe();
+    };
+  }, []);
 
-    return (
-        <div className={css.UIKeyboard}>
-            <label htmlFor="octave">Octave: </label>
-            <input
-                type="number"
-                id="octave"
-                min="-5"
-                max="5"
-                value={octave}
-                onChange={onOctave}
-            />
+  function onOctave(e: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = e.currentTarget;
 
-            <input
-                id="pitchBend"
-                type="range"
-                min={0} max={16383}
-                value={pitchBend}
-                onChange={onPitchBend}
-                onMouseUp={onPitchBend}
-                onContextMenu={e => e.preventDefault()}
-            />
-            
-            <div className={css.keys}>
-                {keys.map(({ name, value, className, leftOffset }) => {
-                    const isActive =
-                        activeNotes.includes(value + octaveToNoteOffset(octave));
+    setOctave(+value);
+    publishOctaveChange(+value);
+  }
 
-                    return (
-                        <button
-                            type="button"
-                            aria-label={`key-${name}`}
-                            value={value}
-                            key={`key-${name}`}
-                            onMouseDown={onNote}
-                            onMouseUp={onNote}
-                            onMouseLeave={onNote}
-                            style={{ left: `calc(100% * ${leftOffset / 7}` }}
-                            className={`${css.key} ${isActive ? css[`key--active`] : ""} ${css[className]}`}
-                            data-testid={isActive ? `key--active` : ''}
-                        >
-                        </button>
-                    )
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div className={css.UIKeyboard}>
+      <label htmlFor="octave">Octave: </label>
+      <input
+        type="number"
+        id="octave"
+        min="-5"
+        max="5"
+        value={octave}
+        onChange={onOctave}
+      />
+
+      <input
+        id="pitchBend"
+        type="range"
+        min={0}
+        max={16383}
+        value={pitchBend}
+        onChange={onPitchBend}
+        onMouseUp={onPitchBend}
+        onContextMenu={(e) => e.preventDefault()}
+      />
+
+      <div className={css.keys}>
+        {keys.map(({ name, value, className, leftOffset }) => {
+          const isActive = activeNotes.includes(
+            value + octaveToNoteOffset(octave)
+          );
+
+          return (
+            <button
+              type="button"
+              aria-label={`key-${name}`}
+              value={value}
+              key={`key-${name}`}
+              onMouseDown={onNote}
+              onMouseUp={onNote}
+              onMouseLeave={onNote}
+              style={{ left: `calc(100% * ${leftOffset / 7}` }}
+              className={`${css.key} ${isActive ? css[`key--active`] : ""} ${
+                css[className]
+              }`}
+              data-testid={isActive ? `key--active` : ""}
+            ></button>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export { UIKeyboard };
