fe1c790cd6a1baecab68a37efbf2b0cf99d46719
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-05-02 12:43:00 +0100

    refactor: Rename type ScaleIndex to ScalerMap

--- src/Synth/index.ts
+++ src/Synth/index.ts
@@ -1,19 +1,19 @@
-import * as Tone from 'tone';
+import * as Tone from "tone";
 import {
-    ControlChange, 
-    ControlChangeSubscriber, 
-    NoteOff, 
-    NoteOffSubscriber, 
-    NoteOn, 
-    NoteOnSubscriber,
-    OctaveChangeSubscriber,
-    PitchBendSubscriber,
-    FocusChangeSubscriber
-} from '../PubSub';
-import * as scaler from '../utils/Scalers';
+  ControlChange,
+  ControlChangeSubscriber,
+  NoteOff,
+  NoteOffSubscriber,
+  NoteOn,
+  NoteOnSubscriber,
+  OctaveChangeSubscriber,
+  PitchBendSubscriber,
+  FocusChangeSubscriber,
+} from "../PubSub";
+import * as scaler from "../utils/Scalers";
 
-interface ScaleIndex {
-    [key: string]: (value: number) => void;
+interface ScalerMap {
+  [key: string]: (value: number) => void;
 }
 
 const SYNTH = new Tone.PolySynth(Tone.MonoSynth);
@@ -23,6 +23,8 @@ console.log(SYNTH.get());
 let activeNotes: number[] = [];
 let focus = "";
 let octave = 0;
+const minOctave = -5;
+const maxOctave = 5;
 
 new NoteOnSubscriber(onNoteOn);
 new NoteOffSubscriber(onNoteOff);
@@ -32,123 +34,128 @@ new OctaveChangeSubscriber(onOctaveChange);
 new PitchBendSubscriber(onPitchBend);
 
 function onNoteOn(topic: string, data: NoteOn) {
-    if (activeNotes.includes(data.noteNumber)) return;
+  const noteNumber = data.noteNumber + octave * 12;
 
-    const volume = (1 / 127) * data.velocity;
-    activeNotes = [data.noteNumber, ...activeNotes];
+  if (noteNumber < 0 || noteNumber > 127) return;
+  if (activeNotes.includes(noteNumber)) return;
 
+  const volume = (1 / 127) * data.velocity;
+  activeNotes = [noteNumber, ...activeNotes];
 
-    SYNTH.triggerAttack(
-        Tone.Frequency(data.noteNumber, 'midi').toNote(),
-        Tone.now(),
-        volume
-    );
+  SYNTH.triggerAttack(
+    Tone.Frequency(noteNumber, "midi").toNote(),
+    Tone.now(),
+    volume
+  );
 }
 
 function onNoteOff(topic: string, data: NoteOff) {
-    if (!activeNotes.includes(data.noteNumber)) return;
+  const noteNumber = data.noteNumber + octave * 12;
+  if (noteNumber < 0 || noteNumber > 127) return;
+  if (!activeNotes.includes(noteNumber)) return;
 
-    activeNotes = activeNotes.filter(note => note !== data.noteNumber);
+  activeNotes = activeNotes.filter((note) => note !== noteNumber);
 
-
-    SYNTH.triggerRelease(Tone.Frequency(data.noteNumber, 'midi').toNote());
+  SYNTH.triggerRelease(Tone.Frequency(noteNumber, "midi").toNote());
 }
 
 function onFocusChange(topic: string, data: string) {
-    focus = data;
+  focus = data;
 }
 
-
 function onControlChange(topic: string, data: ControlChange) {
-    if (focus === "filter") return updateFilter(data);
-    if (focus === "filterEnvelope") return updateFilterEnvelope(data);
-    if (focus === "ampEnvelope") return updateAmpEnvelope(data);
+  if (focus === "filter") return updateFilter(data);
+  if (focus === "filterEnvelope") return updateFilterEnvelope(data);
+  if (focus === "ampEnvelope") return updateAmpEnvelope(data);
 }
 
 function updateFilter(data: ControlChange) {
-    const { controlChangeNumber: cc, value } = data;
+  const { controlChangeNumber: cc, value } = data;
 
-    if (cc === 70) return updateFilterFrequency(value);
-    if (cc === 71) return updateFilterResonance(value);
+  if (cc === 70) return updateFilterFrequency(value);
+  if (cc === 71) return updateFilterResonance(value);
 }
 
 function updateFilterFrequency(value: number) {
-    SYNTH.set({
-        filterEnvelope: {
-            baseFrequency: scaler.controlChangeToFilterFrequency(value)
-        }
-    });
+  SYNTH.set({
+    filterEnvelope: {
+      baseFrequency: scaler.controlChangeToFilterFrequency(value),
+    },
+  });
 }
 
 function updateFilterResonance(value: number) {
-    SYNTH.set({
-        filter: {
-            Q: scaler.controlChangeToFilterResonance(value)
-        }
-    })
+  SYNTH.set({
+    filter: {
+      Q: scaler.controlChangeToFilterResonance(value),
+    },
+  });
 }
 
 function updateFilterEnvelope(data: ControlChange) {
-    const { controlChangeNumber: cc, value } = data;
+  const { controlChangeNumber: cc, value } = data;
 
-    if (cc === 70) return updateFilterEnvelopeParam("attack", value);
-    if (cc === 71) return updateFilterEnvelopeParam("decay", value);
-    if (cc === 72) return updateFilterEnvelopeParam("sustain", value);
-    if (cc === 73) return updateFilterEnvelopeParam("release", value);
-    if (cc === 74) return updateFilterEnvelopeParam("amount", value);
+  if (cc === 70) return updateFilterEnvelopeParam("attack", value);
+  if (cc === 71) return updateFilterEnvelopeParam("decay", value);
+  if (cc === 72) return updateFilterEnvelopeParam("sustain", value);
+  if (cc === 73) return updateFilterEnvelopeParam("release", value);
+  if (cc === 74) return updateFilterEnvelopeParam("amount", value);
 }
 
 function updateFilterEnvelopeParam(param: string, value: number) {
-    const scalers: ScaleIndex = {
-        attack: scaler.controlChangeToFilterEnvelopeAttack,
-        decay: scaler.controlChangeToFilterEnvelopeDecay,
-        sustain: scaler.controlChangeToFilterEnvelopeSustain,
-        release: scaler.controlChangeToAmpEnvelopeRelease,
-        amount: scaler.controlChangeToFilterEnvelopeAmount,
-    }
+  const scalerMap: ScalerMap = {
+    attack: scaler.controlChangeToFilterEnvelopeAttack,
+    decay: scaler.controlChangeToFilterEnvelopeDecay,
+    sustain: scaler.controlChangeToFilterEnvelopeSustain,
+    release: scaler.controlChangeToAmpEnvelopeRelease,
+    amount: scaler.controlChangeToFilterEnvelopeAmount,
+  };
 
-    SYNTH.set({
-        filterEnvelope: {
-            [param]: scalers[param](value)
-        }
-    });
+  SYNTH.set({
+    filterEnvelope: {
+      [param]: scalerMap[param](value),
+    },
+  });
 }
 
 function updateAmpEnvelope(data: ControlChange) {
-    const { controlChangeNumber: cc, value } = data;
+  const { controlChangeNumber: cc, value } = data;
 
-    if (cc === 70) return updateAmpEnvelopeParam("attack", value);
-    if (cc === 71) return updateAmpEnvelopeParam("decay", value);
-    if (cc === 72) return updateAmpEnvelopeParam("sustain", value);
-    if (cc === 73) return updateAmpEnvelopeParam("release", value);
+  if (cc === 70) return updateAmpEnvelopeParam("attack", value);
+  if (cc === 71) return updateAmpEnvelopeParam("decay", value);
+  if (cc === 72) return updateAmpEnvelopeParam("sustain", value);
+  if (cc === 73) return updateAmpEnvelopeParam("release", value);
 }
 
 function updateAmpEnvelopeParam(param: string, value: number) {
+  const scalerMap: ScalerMap = {
+    attack: scaler.controlChangeToAmpEnvelopeAttack,
+    decay: scaler.controlChangeToAmpEnvelopeDecay,
+    sustain: scaler.controlChangeToAmpEnvelopeSustain,
+    release: scaler.controlChangeToAmpEnvelopeRelease,
+  };
 
-    const scalers: ScaleIndex = {
-        attack: scaler.controlChangeToAmpEnvelopeAttack,
-        decay: scaler.controlChangeToAmpEnvelopeDecay,
-        sustain: scaler.controlChangeToAmpEnvelopeSustain,
-        release: scaler.controlChangeToAmpEnvelopeRelease
-    };
-
-    SYNTH.set({
-        envelope: {
-            [param]: scalers[param](value)
-        }
-    });
+  SYNTH.set({
+    envelope: {
+      [param]: scalerMap[param](value),
+    },
+  });
 }
 
 function onOctaveChange(topic: string, data: number) {
-    octave = data;
+  if (data < minOctave || data > maxOctave) return;
+  SYNTH.releaseAll()
+  octave = data;
 }
 
 function onPitchBend(topic: string, data: number) {
-    const pitchBend = data;
+  const pitchBend = data;
 
-    const detune = scaler.pitchBendToDetune(data, 1200);
+  const detune = scaler.pitchBendToDetune(data, 1200);
 
-    SYNTH.set({
-        detune: detune,
-    })
+  SYNTH.set({
+    detune: detune,
+  });
 }
+
+export { octave, minOctave, maxOctave };
